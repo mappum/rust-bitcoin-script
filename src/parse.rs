390dbaa22fcb5fa251caec9ec3e59e970ef5103d
Matt Bell <mappum@gmail.com>
2020-03-02 16:34:27 -0800

    Added parse unit tests

--- src/parse.rs
+++ src/parse.rs
@@ -1,7 +1,6 @@
 use bitcoin::blockdata::opcodes::{All as Opcode, all as opcodes};
 use lazy_static::lazy_static;
 use proc_macro2::{TokenTree::{self, *}, TokenStream, Span};
-use proc_macro_error::{abort, emit_error};
 use std::collections::HashMap;
 
 // index opcodes by identifier string
@@ -25,6 +24,26 @@ pub enum Syntax {
     Int(i64)
 }
 
+macro_rules! emit_error {
+    ($span:expr, $($message:expr),*) => {{
+        #[cfg(not(test))]
+        proc_macro_error::emit_error!($span, $($message),*);
+
+        #[cfg(test)]
+        panic!($($message),*);
+    }}
+}
+
+macro_rules! abort {
+    ($span:expr, $($message:expr),*) => {{
+        #[cfg(not(test))]
+        proc_macro_error::abort!($span, $($message),*);
+
+        #[cfg(test)]
+        panic!($($message),*);
+    }}
+}
+
 pub fn parse(tokens: TokenStream) -> Vec<(Syntax, Span)> {
     let mut tokens = tokens.into_iter();
     let mut syntax = Vec::with_capacity(64);
@@ -35,11 +54,14 @@ pub fn parse(tokens: TokenStream) -> Vec<(Syntax, Span)> {
         syntax.push(match (&token, token_str.as_ref()) {
             // identifier, look up opcode
             (Ident(_), _) => {
-                let opcode = OPCODES.get(&token_str)
-                    .unwrap_or_else(|| {
-                        emit_error!(token.span(), "unknown opcode");
-                        &opcodes::OP_NOP
-                    });
+                let opcode = OPCODES.get(&token_str).unwrap_or_else(|| {
+                    emit_error!(
+                       token.span(),
+                       "unknown opcode \"{}\"",
+                       token_str
+                    );
+                    &opcodes::OP_NOP
+                });
                 (Syntax::Opcode(*opcode), token.span())
             },
 
@@ -135,5 +157,126 @@ fn parse_negative_int<T>(token: TokenTree, tokens: &mut T) -> (Syntax, Span)
         }
     } else {
         fail()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bitcoin::blockdata::opcodes::all as opcodes;
+    use proc_macro2::TokenTree;
+    use quote::quote;
+    use super::*;
+
+    #[test]
+    fn parse_empty() {
+        assert!(parse(quote!()).is_empty());
+    }
+
+    #[test]
+    #[should_panic(expected = "unexpected token")]
+    fn parse_unexpected_token() {
+        parse(quote!(OP_CHECKSIG &));
+    }
+
+    #[test]
+    #[should_panic(expected = "unknown opcode \"A\"")]
+    fn parse_invalid_opcode() {
+        parse(quote!(OP_CHECKSIG A B));
+    }
+
+    #[test]
+    fn parse_opcodes() {
+        let syntax = parse(quote!(OP_CHECKSIG OP_HASH160));
+
+        if let Syntax::Opcode(opcode) = syntax[0].0 {
+            assert_eq!(opcode, opcodes::OP_CHECKSIG);
+        } else {
+            panic!();
+        }
+
+        if let Syntax::Opcode(opcode) = syntax[1].0 {
+            assert_eq!(opcode, opcodes::OP_HASH160);
+        } else {
+            panic!();
+        }
+    }
+    
+    #[test]
+    #[should_panic(expected = "unterminated escape")]
+    fn parse_unterminated_escape() {
+        parse(quote!(OP_CHECKSIG <abc));
+    }
+
+    #[test]
+    fn parse_escape() {
+        let syntax = parse(quote!(OP_CHECKSIG <abc>));
+
+        if let Syntax::Escape(tokens) = &syntax[1].0 {
+            let tokens = tokens
+                .clone()
+                .into_iter()
+                .collect::<Vec<TokenTree>>();
+
+            assert_eq!(tokens.len(), 1);
+            if let TokenTree::Ident(_) = tokens[0] {
+                assert_eq!(tokens[0].to_string(), "abc");
+            } else {
+                panic!()
+            }
+        } else {
+            panic!()
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "invalid number literal (invalid digit found in string)")]
+    fn parse_invalid_int() {
+        parse(quote!(OP_CHECKSIG 12g34));
+    }
+
+    #[test]
+    fn parse_int() {
+        let syntax = parse(quote!(OP_CHECKSIG 1234));
+
+        if let Syntax::Int(n) = syntax[1].0 {
+            assert_eq!(n, 1234i64);
+        } else {
+            panic!()
+        }
+    }
+
+
+    #[test]
+    #[should_panic(expected = "expected negative sign to be followed by number literal")]
+    fn parse_invalid_negative_sign() {
+        parse(quote!(OP_CHECKSIG - OP_HASH160));
+    }
+
+    #[test]
+    fn parse_negative_int() {
+        let syntax = parse(quote!(OP_CHECKSIG -1234));
+
+        if let Syntax::Int(n) = syntax[1].0 {
+            assert_eq!(n, -1234i64);
+        } else {
+            panic!()
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "invalid hex literal (Odd number of digits)")]
+    fn parse_invalid_hex() {
+        parse(quote!(OP_CHECKSIG 0x123));
+    }
+
+    #[test]
+    fn parse_hex() {
+        let syntax = parse(quote!(OP_CHECKSIG 0x1234));
+
+        if let Syntax::Bytes(bytes) = &syntax[1].0 {
+            assert_eq!(bytes, &vec![ 0x12, 0x34 ]);
+        } else {
+            panic!()
+        }
     }
 }
